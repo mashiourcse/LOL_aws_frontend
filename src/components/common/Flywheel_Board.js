995fff10e916ca2b9b9030ee25b4998d52b40f0a
mashiourcse <mashiour.cseiiuc@gmail.com>
2023-10-03 22:57:46 +0600

    added ranking option(win, loss)

--- src/components/common/Flywheel_Board.js
+++ src/components/common/Flywheel_Board.js
@@ -1,21 +1,65 @@
-import React from 'react'
-import { Leaderboard } from 'flywheel-leaderboard'
-export const Flywheel_Board = ({data}) => {
+import React, { useEffect, useState } from 'react';
+import { Leaderboard } from 'flywheel-leaderboard';
+
+export const Flywheel_Board = ({ data }) => {
+  const [selectedRanking, setSelectedRanking] = useState('win');
+
+  const ranking = ['win', 'loss'];
+
+  useEffect(() => {
+    sortingRanking();
+  }, [selectedRanking, data]);
+
+  const sortingRanking = () => {
+    const sortedData = [...data];
+    
+    sortedData.sort((a,b)=> b[selectedRanking] - a[selectedRanking])
+    // if (selectedRanking === 'loss') {
+    //   sortedData.sort((a, b) => b.loss - a.loss);
+    // } else if (selectedRanking === 'win') {
+    //   sortedData.sort((a, b) => b.win - a.win);
+    // }
+
+    // Update the rank
+    sortedData.forEach((team, index) => {
+      team.rank = index + 1;
+    });
+
+    return sortedData;
+  };
+
+  const changeRank = (e) => {
+    setSelectedRanking(e.target.value);
+  };
+
   return (
-    <div >
-        <Leaderboard 
-        
-        className='max-w-4xl' //tailwind class (optional)
-        theme='stone' //leaderboard theme. see docs for accepted values (optional)
-      //  scoringMetric="rank" //the property you wanna rank your data by (required)
-        //id="_id" //the property you wanna id each item in your data by (required)
-        cell1="team_id" //the first cell for your board (optional)
-        cell2="team_code" //...
-        cell3="team_name" //...
-        cell4="rank" //...
-        items={data} //the data you wanna use for your board. e.g. db response. (required)
-        > 
-      </Leaderboard>
+    <div>
+      <div>
+        Ranking by{' '}
+        <select
+          value={selectedRanking}
+          onChange={changeRank}
+          className="mb-2 w-30 px-4 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+        >
+          {ranking.map((value) => (
+            <option key={value} value={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Leaderboard
+        className="max-w-4xl"
+        theme="stone"
+        scoringMetric={selectedRanking}
+        //id="_id"
+        cell1="team_id"
+        cell2="team_code"
+        cell3="team_name"
+        cell4="rank"
+        cell5={selectedRanking}
+        items={sortingRanking()}
+      />
     </div>
-  )
-}
+  );
+};
